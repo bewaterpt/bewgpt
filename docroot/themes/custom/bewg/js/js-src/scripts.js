9a4e91bc004f71b205bb97e2b2bd569c53aebe73
Fabio Santos <fabio.santos@bewgpt.com.pt>
2020-12-07 20:36:44 +0000

    changes menu

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -1,10 +1,22 @@
 
-if(document.getElementsByClassName("menu-item--expanded")){
-    alert("entrou");
-    document.getElementsByClassName("menu-item--expanded").onclick=function(event){
+(function($){
+    $(document).on("click",function(event){
+        console.log(event.target)
+    })
 
-        alert("funca");
-    };
-};
+    $("#block-mainnavigation ul>.menu-item").on("click",function(event){
+        event.preventDefault();
+        console.log($(event.currentTarget).find(".menu"));
+        if($(event.currentTarget).find(".menu")!= ""){
+            $(event.currentTarget).find(".menu").toggleClass("active");
+        }
+        else{
+            $(event.currentTarget).find(".active").removeClass("menu");  
+        }
+        
+    })
+
+
+})(jQuery)
 
 
--- docroot/themes/custom/bewg/js/scripts.js
+++ docroot/themes/custom/bewg/js/scripts.js
@@ -1,2 +1,2 @@
-document.getElementsByClassName("menu-item--expanded")&&(alert("entrou"),document.getElementsByClassName("menu-item--expanded").onclick=function(e){alert("funca")});
+!function(e){e(document).on("click",function(e){console.log(e.target)}),e("#block-mainnavigation ul>.menu-item").on("click",function(n){n.preventDefault(),console.log(n.currentTarget),e(n.currentTarget).find(".menu")?e(n.currentTarget).find(".menu").toggleClass("active"):e(n.currentTarget).find(".active").removeClass("menu")})}(jQuery);
 //# sourceMappingURL=maps/scripts.js.map
