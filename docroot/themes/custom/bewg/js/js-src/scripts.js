15173f404ab71c10d486451da3dc9172931b82bb
Fabio Santos <fabio.santos@bewgpt.com.pt>
2020-12-10 09:36:29 +0000

    Chnages Menu

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -1,18 +1,22 @@
 
 (function($){
-    $(document).on("click",function(event){
-        console.log(event.target)
-    })
 
     $("#block-mainnavigation ul>.menu-item").on("click",function(event){
-        event.preventDefault();
-        console.log($(event.currentTarget).find(".menu"));
-        if($(event.currentTarget).find(".menu")!= ""){
+        //verificar se o elemento tem ul, se tiver da prevent
+        if ( $(event.currentTarget).children("ul").length > 0 && $(event.currentTarget).children("li").length == 0) {
+            console.log("ver: "+$(event.currentTarget).children("ul").length); 
+            console.log("clicked: "+$(event.target).text());
+            event.preventDefault();
+            $(event.currentTarget).toggleClass("menuDrop");
             $(event.currentTarget).find(".menu").toggleClass("active");
+            alert("erro")
         }
         else{
-            $(event.currentTarget).find(".active").removeClass("menu");  
+           alert("certo")
+           console.log("ver: "+$(event.currentTarget).children("ul").length); 
         }
+        
+        
         
     })
 
