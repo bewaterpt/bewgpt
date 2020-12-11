9cf80d8a46c2100aa6ffb7806e1f3a42e126b57d
Fabio Santos <fabio.santos@bewgpt.com.pt>
2020-12-11 18:50:20 +0000

    changes background_image

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -8,26 +8,9 @@
             event.preventDefault();
             $(event.currentTarget).toggleClass("menuDrop");
             $(event.currentTarget).find(".menu").toggleClass("active");
-            console.log($(event.currentTarget).children("ul").children("li").length);
-            $(".menuDrop").css("max-height", ($(event.currentTarget).children("ul").children("li").length) * 55+"px")
-            $(".active").css("height", ($(event.currentTarget).children("ul").children("li").length) * 44+"px")
-            
+                
         }   
     })
-
-    //Abrir submenu debaixo do menu-item
-    
-    // var menuitemCounter=document.getElementsByClassName("menu-item");
-
-    // $("#block-mainnavigation ul>.menu-item")
-    // for(var i =0; i<menuitemCounter.length;i++){
-    //     menuitemCounter[i].onclick=function(event){
-    //         event.preventDefault();
-    //         console.log(i);
-    //         $(".active").css("margin-top", (i * 44+"px"));
-    //     }
-    // }
-
 
 })(jQuery)
 
