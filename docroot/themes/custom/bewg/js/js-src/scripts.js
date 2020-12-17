ef31c60b64d8f7d9794c9fa5aeee28198ce018e9
Fabio Santos <fabio.santos@bewgpt.com.pt>
2020-12-17 21:28:38 +0000

    changes menubar/webforms

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -11,12 +11,25 @@
                
         }   
     });
-
+    //botao do menu abrir
     $("#menuBar_btn").on("click",function(){
         $("#sidebar-first").toggleClass("sidebar_inactive");
         $("#content").toggleClass("content_active");
         $(".menuBar_btn").toggleClass("menuBar_btn_active");
     });
+
+    $(document).mouseup(function(e){
+        var container = $("#sidebar-first");
+        console.log("entrei");
+        // If the target of the click isn't the container
+        if(!container.is(e.target) && container.has(e.target).length === 0 && container.width()>0){
+            container.removeClass("sidebar_inactive");
+            $("#content").removeClass("content_active");
+            $(".menuBar_btn").removeClass("menuBar_btn_active");
+           
+        }
+    });
+
 
     $('#caroussel').slick();
 
