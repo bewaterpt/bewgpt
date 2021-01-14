9353435e74060dd77b635d1b2f6a123da44fd72b
Fabio Santos <fabio.santos@bewgpt.com.pt>
2021-01-14 16:22:20 +0000

    Changes image width

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -87,6 +87,17 @@
   //     }
   //   }, 10);
   // }
+
+  $('#content').lazy({
+    // your configuration goes here
+    scrollDirection: 'vertical',
+    effect: 'fadeIn',
+    visibleOnly: true,
+    onError: function(element) {
+        console.log('error loading ' + element.data('src'));
+    }
+  });
+
 })(jQuery)
 
 
