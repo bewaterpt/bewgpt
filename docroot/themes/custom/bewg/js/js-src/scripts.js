e15f7d68ae47802bf4b563da86e1e8729d03fe6b
Fabio Santos <fabio.santos@bewgpt.com.pt>
2021-01-18 10:01:26 +0000

    Changes LazyLoad

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -90,16 +90,7 @@
 
 
   window.addEventListener('load', (event) => {
-    console.log("true");
-    $('#content').lazy({
-      // your configuration goes here
-      scrollDirection: 'vertical',
-      effect: 'fadeIn',
-      visibleOnly: true,
-      onError: function(element) {
-          console.log('error loading ' + element.data('src'));
-      }
-    });
+
   });
 
 })(jQuery)
