b5da7d158e7cec51d9e677956b7311402c3a164d
Fabio Santos <fabio.santos@bewgpt.com.pt>
2021-01-18 12:04:39 +0000

    Changes LazyLoad

--- docroot/themes/custom/bewg/js/js-src/scripts.js
+++ docroot/themes/custom/bewg/js/js-src/scripts.js
@@ -87,15 +87,7 @@
   //     }
   //   }, 10);
   // }
-  $('#content').Lazy({
-    // your configuration goes here
-    scrollDirection: 'vertical',
-    effect: 'fadeIn',
-    visibleOnly: true,
-    onError: function(element) {
-        console.log('error loading ' + element.data('src'));
-    }
-  });
+
 
 
 })(jQuery)
